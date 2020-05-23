47a6d79fa01d4f80c64edf3ee969290a05f42f58
Isaiah Koh <zaiah0505@hotmail.com>
2020-05-24 00:10:17 +0800

    added placeholder rule API

---
+++ server/index2.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+//--Declaring dependencies
+//Body parser is used to automatically include POST HTTP request data in the
+//request body
+app.use(bodyParser.urlencoded({ extended: true}))
+
+//Get the specified local database
+function getDatabase(dbName) {
+  return new Promise((resolve, reject) => {
+    const client_uri = "mongodb://127.0.0.1:27017";
+
+    MongoClient.connect(client_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(client => {
+      resolve(client.db(dbName));
+    })
+    .catch(err => reject(err));
+  })
+}
+
+//Takes in a collection reference and module code string and returns a Promise
+async function getModule(collection, module){
+  const one = await collection.find({'moduleCode': module});
+  const two = await one.toArray();
+  const output = await two[0];
+  return output;
+}
+
+//Takes in a collection reference and a rule tag string and returns a Promise
+async function getRule(collection, ruleTag){
+  var cursor = await collection.find({'tag': ruleTag});
+  var array = await cursor.toArray();
+  return array[0];
+}
+
+//Rule constructor: generates a full JS Object base on a rule in the database
+async function createRule(rule) {
+  if (rule.startsWith('r_')){
+    const db = await getDatabase('modtree');
+    const col = db.collection('rules');
+    var current = await getRule(col, rule);
+    
+    var key = Object.keys(current.requirements)[0];
+    var req = current.requirements[key];
+    req = await Promise.all(req.map(item => createRule(item)));
+    current.requirements[key] = req;    
+
+    return current;
+  } else {
+    const db = await getDatabase('modtree');
+    const output = await getModule(db.collection('modules'), rule);
+    return output;
+  }  
+}
+
+//Route handlers
+app.get('/', function (req, res) {
+  createRule('r_bachelor_cs_2018')
+  .then(result => res.send(result));
+  //res.send(createRule('r_cs_foundation'));
+});
+
+app.get('/express_backend', function (req, res) {
+  res.send({express: 'This line came from the Express server'});
+});
+
+app.listen(3000, function () {
+  console.log('Express server is listening on port 5000!');
+});
